c60fbd9cb50cb85135288054f1de00a42480bc64
MetalSnow <aymanetrend@gmail.com>
2026-07-13 01:34:46 +0800

    feat(profile): add file upload with Multer and Supabase integration
    - Add Multer middleware to profile patch route for file uploads
    - Implement Supabase storage for profile and cover picture uploads
    - Update profile controller to handle multipart form data
    - Return updated user data with image URLs to frontend

--- backend/controllers/profile.controller.js
+++ backend/controllers/profile.controller.js
@@ -1,5 +1,12 @@
 const asyncHandler = require('express-async-handler');
 const prisma = require('../config/prismaClient');
+const { createClient } = require('@supabase/supabase-js');
+
+require('dotenv').config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const getProfile = asyncHandler(async (req, res) => {
   const username = req.params.username;
@@ -21,7 +28,37 @@ const getProfile = asyncHandler(async (req, res) => {
 
 const editProfile = asyncHandler(async (req, res) => {
   const userId = Number(req.params.id);
-  const { bio, gender, profilePic, coverPic } = req.body;
+  const { bio, gender } = req.body;
+  const profilePic = req.files['profilePic'];
+  const coverPic = req.files['coverPic'];
+  let profilePath;
+  let coverPath;
+
+  if (profilePic) {
+    profilePath = `${userId}/${Date.now()}-${profilePic[0].originalname}`;
+
+    const { data, error } = await supabase.storage
+      .from('avatar')
+      .upload(profilePath, profilePic[0].buffer);
+
+    if (error) {
+      console.error('Upload error:', error);
+      return next(error);
+    }
+  }
+
+  if (coverPic) {
+    coverPath = `${userId}/${Date.now()}-${coverPic[0].originalname}`;
+
+    const { data, error } = await supabase.storage
+      .from('cover')
+      .upload(coverPath, coverPic[0].buffer);
+
+    if (error) {
+      console.error('Upload error:', error);
+      return next(error);
+    }
+  }
 
   const data = await prisma.profile.update({
     where: {
@@ -30,8 +67,8 @@ const editProfile = asyncHandler(async (req, res) => {
     data: {
       bio,
       gender,
-      profilePic,
-      coverPic,
+      profilePic: profilePath,
+      coverPic: coverPath,
     },
   });
 
--- backend/routes/profileRouter.js
+++ backend/routes/profileRouter.js
@@ -4,12 +4,21 @@ const {
   getProfile,
   editProfile,
 } = require('../controllers/profile.controller');
+const multer = require('multer');
+const upload = multer({ storage: multer.memoryStorage() });
 
 const profileRouter = express.Router();
 
 profileRouter.use(isAuth);
 
 profileRouter.get('/profile/:username', getProfile);
-profileRouter.patch('/profile/:id', editProfile);
+profileRouter.patch(
+  '/profile/:id',
+  upload.fields([
+    { name: 'profilePic', maxCount: 1 },
+    { name: 'coverPic', maxCount: 1 },
+  ]),
+  editProfile,
+);
 
 module.exports = profileRouter;

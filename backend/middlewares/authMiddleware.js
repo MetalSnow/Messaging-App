cf4a9b05a69520f778f28df2521edeece50ea32c
MetalSnow <aymanetrend@gmail.com>
2026-08-20 22:53:04 +0800

    fix(isAuth): send res with status 200 instead of 401

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -12,7 +12,7 @@ const isAuth = (req, res, next) => {
   if (req.isAuthenticated()) {
     next();
   } else {
-    res.status(401).json({
+    res.json({
       success: false,
       message: 'You are not authorized to view this resource.',
     });
